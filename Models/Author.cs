de9a68f111b89160a2d08767a7b34464396c931b
amarGhandour <30914748+amarGhandour@users.noreply.github.com>
2023-05-07 15:31:35 +0300

    Author Model Added

---
+++ Models/Author.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Book_API.Models
+{
+    public class Author
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+
+        public string? Biographpy { get; set; }
+
+        public string? ImageName { get; set; }
+
+
+
+
+    }
+}
