c5fed7603772894c318b2d09e2146454e72d469e
kareemallam <98554346+amriallam@users.noreply.github.com>
2023-05-07 15:30:27 +0300

    Category  Model Book Model

---
+++ Models/Book.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection.Metadata;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int NumberOfPages{ get; set; }
+    public List<Category> Categories { get; set; }
+    public Blob Image { get; set; }
+}
